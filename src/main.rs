a63e22d6cc3773fc325ee4aaef345298c469a57d
Todd W Crone <twcrone@gmail.com>
2021-11-22 22:06:30 -0500

    Add the player

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod map;
+mod player;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -6,24 +7,31 @@ mod prelude {
     pub const FRAME_DURATION : f32 = 75.0;
     pub const SCREEN_WIDTH : i32 = 80;
     pub use crate::map::*;
+    pub use crate::player::*;
 }
 
 use prelude::*;
 
 struct State {
     map: Map,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
-        Self { map: Map::new() }
+        Self {
+            map: Map::new(),
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+        }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
