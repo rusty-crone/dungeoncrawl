d22050820daa26992b1189be6eadfeab8fd7b32e
Todd W Crone <twcrone@gmail.com>
2021-11-22 20:15:25 -0500

    Initial dungeon map

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
-use bracket_lib::prelude::*;
+mod map;
 
-fn main() {
-    println!("Hello, world!");
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_HEIGHT : i32 = 50;
+    pub const FRAME_DURATION : f32 = 75.0;
+    pub const SCREEN_WIDTH : i32 = 80;
+    pub use crate::map::*;
+}
+
+use prelude::*;
+
+struct State {
+    map: Map,
+}
+
+impl State {
+    fn new() -> Self {
+        Self { map: Map::new() }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.map.render(ctx);
+    }
+}
+
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
+        .build()?;
+    main_loop(context, State::new())
 }
