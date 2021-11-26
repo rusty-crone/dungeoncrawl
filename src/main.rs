eadeb227871610a57efb91147140b8726173ff34
Todd W Crone <twcrone@gmail.com>
2021-11-26 11:06:16 -0500

    Build the random map

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod map;
+mod map_builder;
 mod player;
 
 mod prelude {
@@ -8,6 +9,7 @@ mod prelude {
     pub const SCREEN_WIDTH : i32 = 80;
     pub use crate::map::*;
     pub use crate::player::*;
+    pub use crate::map_builder::*;
 }
 
 use prelude::*;
@@ -19,9 +21,11 @@ struct State {
 
 impl State {
     fn new() -> Self {
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
         Self {
-            map: Map::new(),
-            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start)
         }
     }
 }
