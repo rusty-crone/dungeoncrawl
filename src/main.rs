cb19ebbc5beb7325c9c3712b12b268b5e672b5ac
Todd W Crone <tcrone@newrelic.com>
2023-07-28 17:00:14 -0400

    yay, dungeon shows up again!

--- src/main.rs
+++ src/main.rs
@@ -59,7 +59,7 @@ impl GameState for State {
         ctx.cls();
         self.resources.insert(ctx.key);
         self.systems.execute(&mut self.ecs, &mut self.resources);
-        // TODO: Render Draw Buffer
+        render_draw_buffer(ctx).expect("Render error");
     }
 }
 
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,8 +1,10 @@
+mod map_render;
 mod player_input;
 use crate::prelude::*;
 
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(map_render::map_render_system())
         .build()
 }
