da09e729785e35212ba708b6a2421af9caa1e7f4
T.W. Crone <twcrone@gmail.com>
2025-03-17 19:44:27 -0400

    Checkpoint on systems...

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,9 +1,11 @@
 use crate::prelude::*;
 
 mod player_input;
+mod map_renderer;
 
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(map_renderer::map_renderer_system())
         .build()
 }
