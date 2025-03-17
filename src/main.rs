7f8b703aa947e80d2c15356c101d4f88ac89a4a8
T.W. Crone <twcrone@gmail.com>
2025-03-17 19:57:51 -0400

    Fix game window size

--- src/main.rs
+++ src/main.rs
@@ -67,6 +67,7 @@ fn main() -> BError {
         .with_font("dungeonfont.png", 32, 32)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
         .build()?;
     main_loop(context, State::new())
 }
